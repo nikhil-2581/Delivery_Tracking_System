3521936673d0176d2ef68549862c71497f11b8c9
nikhil-2581 <91817141+nikhil-2581@users.noreply.github.com>
2025-12-14 23:38:53 +0530

    updated secret key

--- user-service/src/main/java/com/cryptic/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/cryptic/userservice/UserServiceApplication.java
@@ -2,12 +2,22 @@ package com.cryptic.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of("SYSTEM");
+    }
 }
